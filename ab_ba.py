b4709253dbaa9cdaf6e296f87c28258583ec7fc6
TonyPython0505 <87828913+TonyPython0505@users.noreply.github.com>
2021-12-06 09:32:10 -0700

    Update ab_ba.py

--- ab_ba.py
+++ ab_ba.py
@@ -1,29 +1,29 @@
-sotestcase = int(input())
+n_test_case = int(input())
 results = []
 
-for _ in range(sotestcase):
+for _ in range(n_test_case):
 	string = [c for c in input()]
 	if string == "":
 		results.append("".join(string))
 	else:
-		so_ab = 0
-		so_ba = 0
+		n_ab = 0
+		n_ba = 0
 		for i in range(len(string)-1):
 			if string[i] + string[i+1] == "ab":
-				so_ab += 1
+				n_ab += 1
 			elif string[i] + string[i+1] == "ba":
-				so_ba += 1
-		d = abs(so_ab - so_ba)
+				n_ba += 1
+		d = abs(n_ab - n_ba)
 		if d == 0:
 			results.append("".join(string))
 		else:
-			if so_ab > so_ba:
+			if n_ab > n_ba:
 				if string[0] + string[1] == "ab":
 					string[0] = "b"
 				elif string[-2] + string[-1] == "ab":
 					string[-1] = "a"
 				
-			elif so_ab < so_ba:
+			elif n_ab < n_ba:
 				if string[0] + string[1] == "ba":
 					string[0] = "a"
 				elif string[-2] + string[-1] == "ba":
